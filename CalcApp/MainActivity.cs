72a737b1dad9d12436e5fe610bd4e2ccc1f9679b
WhileTrueDoSex <leshka.mishenko@gmail.com>
2017-04-03 22:50:26 +0500

    some user interface
    user interface was made in material design

--- CalcApp/MainActivity.cs
+++ CalcApp/MainActivity.cs
@@ -1,25 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
 using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Views;
+using Java.Lang;
+using Java.Security;
 using RPN;
 
 namespace CalcApp
 {
-    [Activity(Label = "CalcApp", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(MainLauncher = true, ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
     public class MainActivity : Activity
     {
+        private HorizontalScrollView _hsv;
+        private TextView _primaryDisplay;
+        private TextView _secondaryDisplay;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main);
 
-            var calc = new Calculator();
-            var res = calc.Calculate("1+2");
-            Console.WriteLine(res);
+            _hsv = FindViewById<HorizontalScrollView>(Resource.Id.display_hsv);
+            _primaryDisplay = FindViewById<TextView>(Resource.Id.display_primary);
+            _secondaryDisplay = FindViewById<TextView>(Resource.Id.display_secondary);
+
+            GetDigits().ForEach(x => x.Click += (sender, args) =>
+            {
+                _primaryDisplay.Text += x.Text;
+            });
         }
+
+
+        private List<TextView> GetDigits()
+        {
+            return new List<TextView>
+            {
+                FindViewById<TextView>(Resource.Id.button_0),
+                FindViewById<TextView>(Resource.Id.button_1),
+                FindViewById<TextView>(Resource.Id.button_2),
+                FindViewById<TextView>(Resource.Id.button_3),
+                FindViewById<TextView>(Resource.Id.button_4),
+                FindViewById<TextView>(Resource.Id.button_5),
+                FindViewById<TextView>(Resource.Id.button_6),
+                FindViewById<TextView>(Resource.Id.button_7),
+                FindViewById<TextView>(Resource.Id.button_8),
+                FindViewById<TextView>(Resource.Id.button_9)
+            };
+        }
+
+
     }
+
+     
 }
 
